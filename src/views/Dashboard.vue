193b77e2681a7a36fbd024fdfb268c5d492e7b25
Chamroeun OUM <chamroeunoum@gmail.com>
2026-01-23 15:06:33 +0700

    Discard change in Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="app_wrap">
-    <input type="text" value-model="officer.id" style="border: 1px solid #CCC; " >
-  <!-- <Header />
-  <DashboardGrid /> -->
+  <Header />
+  <DashboardGrid />
   </div>
 </template>
 
-<script >
+<script setup>
 import Header from '@/components/Header.vue'
 import DashboardGrid from "../components/DashboardGrid.vue"
-import { ref } from 'vue'
-import { useStore } from 'vuex'
-  export default {
-    setup() {
-      const store = useStore()
-
-      const officer = ref( null )
-
-      store.dispatch('officer/list').then( res => {
-        officer.value = res.data.records[0]
-      })
-      store.commit('officer/setRecords')
-
-      store.getters['officer/getRecords']
-      return {
-        officer
-      }
-    }
-  }
 </script>
